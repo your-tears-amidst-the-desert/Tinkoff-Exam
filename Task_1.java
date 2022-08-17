1cc6ebebbfc74184a1a529bd466deccae510f7bd
your-tears-amidst-the-desert <110824455+your-tears-amidst-the-desert@users.noreply.github.com>
2022-08-17 23:09:59 +0400

    Create Task_1.java
    Задача про отделы

---
+++ Task_1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Task_1 {
+
+    Task_1() {
+        Scanner scan = new Scanner(System.in);
+        int x1 = scan.nextInt();
+        int y1 = scan.nextInt();
+        int x2 = scan.nextInt();
+        int y2 = scan.nextInt();
+
+        int X1 = scan.nextInt();
+        int Y1 = scan.nextInt();
+        int X2 = scan.nextInt();
+        int Y2 = scan.nextInt();
+
+        int xMin = Math.min(x1, X1);
+        int xMax = Math.max(x2, X2);
+        int yMin = Math.min(y1, Y1);
+        int yMax = Math.max(y2, Y2);
+        int gainX = xMax - xMin;
+        int gainY = yMax - yMin;
+
+        if(gainX > gainY) {
+            System.out.println(gainX * gainX);
+        }
+        else {
+            System.out.println(gainY * gainY);
+        }
+    }
+
+    public static void main(String[] args) {
+        Task_1 t1 = new Task_1();
+    }
+}
