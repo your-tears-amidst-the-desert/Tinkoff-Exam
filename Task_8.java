e246a0fbf985cc0bccf84e62eac69b3370ec922f
your-tears-amidst-the-desert <110824455+your-tears-amidst-the-desert@users.noreply.github.com>
2022-08-17 23:16:34 +0400

    Create Task_8.java
    Задача про домены

---
+++ Task_8.java
@@ -0,0 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task_8 {
+
+    Task_8() {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+
+        scan.nextLine();
+        String[] domens = new String[n];
+        for(int i = 0; i < n; i++) {
+            domens[i] = scan.nextLine().toUpperCase(Locale.ROOT);
+        }
+
+        String[] start = new String[m];
+        String[] finish = new String[m];
+        for(int i = 0; i < m; i++) {
+            start[i] = scan.next();
+            finish[i] = scan.next();
+        }
+
+        for(int i = 0; i < m; i++) {
+            int count = 0;
+            for (String dom : domens) {
+                if (dom.startsWith(start[i].toUpperCase()) & dom.endsWith(finish[i].toUpperCase(Locale.ROOT))) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+
+    public static void main(String[] args) {
+        Task_8 t8 = new Task_8();
+    }
+}
